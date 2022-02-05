eae3dfc03a5d9f2bc91fcc27146c1445840b6cff
Gianluca Ciccarelli <gianluca.ciccarelli@glia.com>
2022-02-05 17:27:58 +0200

    Fill migration, add schema, run migration

---
+++ lib/ecto/places.ex
@@ -0,0 +1,17 @@
+defmodule Playground.Place do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "places" do
+    field(:name, :string)
+    field(:description, :string)
+    field(:visited, :boolean, default: false)
+
+    timestamps()
+  end
+
+  def changeset(record, params \\ :empty) do
+    record
+    |> cast(params, [:name, :description, :visited])
+  end
+end
--- priv/repo/migrations/20220205152352_add_places.exs
+++ priv/repo/migrations/20220205152352_add_places.exs
@@ -1,8 +1,15 @@
 defmodule Playground.Repo.Migrations.AddPlaces do
   use Ecto.Migration
 
-  # Generated using `mix ecto.gen.migration AddPlaces`
+  # Generated using `mix ecto.gen.migration AddPlaces`.
+  # Applied with `ecto.migrate`.
   def change do
+    create table(:places) do
+      add :name, :string
+      add :description, :string
+      add :visited, :boolean, default: false
 
+      timestamps()
+    end
   end
 end
